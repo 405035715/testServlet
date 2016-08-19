de81d22583bd731e87f05cbce786b72b6ca543d5
zyh <zyh@zyh-PC>
2016-08-20 07:07:32 +0800

    new TestLog

---
+++ src/com/zyh/www/TestLog.java
@@ -0,0 +1,10 @@
+package com.zyh.www;
+
+public class TestLog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/com/zyh/www/TestServlet.java
+++ src/com/zyh/www/TestServlet.java
@@ -30,7 +30,7 @@ public class TestServlet extends HttpServlet {
 		String username = request.getParameter("username"); //第一个参数
 		String pwd  = request.getParameter("pwd");  //第二个参数
 		System.out.println( String.format("username : %s, pwd  : %s", username, pwd) );
-		
+//		
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		
 		//打开页面
